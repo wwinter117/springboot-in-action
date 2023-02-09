51fd1ef7eaac593bb7117f0df772f23f8eb3cc04
zhangdd <2067949563@qq.com>
2023-02-09 22:03:30 +0800

    JWT

--- SpringSecurity02_JWT/src/main/java/cn/wwinter/springsecurity02_jwt/service/JwtService.java
+++ SpringSecurity02_JWT/src/main/java/cn/wwinter/springsecurity02_jwt/service/JwtService.java
@@ -2,11 +2,15 @@ package cn.wwinter.springsecurity02_jwt.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 /**
@@ -44,8 +48,37 @@ public class JwtService {
     /**
      * 生成token
      */
-    public String generateToken() {
-        return null;
+    public String generateToken(Map<String, Object> extractClaims, UserDetails userDetails) {
+        return Jwts
+                .builder()
+                .setClaims(extractClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(SignatureAlgorithm.HS256, getSignInKey())
+                .compact();
+    }
+
+    /**
+     * 检查token是否合法
+     */
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    /**
+     * 判断token是否过期
+     */
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    /**
+     * 提取过期时间
+     */
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 
     /**
